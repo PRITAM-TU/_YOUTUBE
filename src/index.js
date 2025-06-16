c3a7c3f2840d5ff9723c5a2187b2b55b292a66c3
Pritam Tung <pritamtung03@gmail.com>
2025-06-16 17:27:51 +0530

    fixed index,js code from chai aur code

--- src/index.js
+++ src/index.js
@@ -1,29 +1,23 @@
 // require('dotenv').config({path: './.env'});
-import dotenv from "dotenv";
-// import mongoose from "mongoose";
-// import { DB_NAME } from "./constants";
+import dotenv from "dotenv"
 import connectDB from "./db/index.js";
-dotenv.config({ path: './.env' });
+import {app} from './app.js'
+dotenv.config({
+    path: './.env'
+})
+
 
 
 connectDB()
 .then(() => {
-     app.on("error", (error) => {
-           console.error("Express server error:", error);
-            throw error;
-       });
-     
-})
-.then(() => {
-     app.listen(process.env.PORT || 8000,()=>{
-        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
     })
-
 })
-.catch((error) => {
-    console.error("MongoDB connection failed:", error);
-    throw error;
-});
+.catch((err) => {
+    console.log("MONGO db connection failed !!! ", err);
+})
+
 
 
 
